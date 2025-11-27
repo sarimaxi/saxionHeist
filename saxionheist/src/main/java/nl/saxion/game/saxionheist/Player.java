5cd12d9b205459c10672724da41c1348f4b7071c
Nancy <nancypagkalis@gmail.com>
2025-11-27 12:18:17 +0100

    added basic player , commented texture,added rect

---
+++ saxionheist/src/main/java/nl/saxion/game/saxionheist/Player.java
@@ -0,0 +1,23 @@
+package nl.saxion.game.saxionheist;
+
+import nl.saxion.gameapp.GameApp;
+
+public class Player {
+    private String textureName;
+    float x, y; // for the position
+
+
+    public Player (String textureName, float x, float y){
+        this.x = x;
+        this.y = y;
+        // GameApp.addTexture(textureName,"player.png");
+    }
+    public void render ( float delta){
+        GameApp.startShapeRenderingFilled();
+        GameApp.drawRect(x, y, 32, 32, "red-500");
+        GameApp.endShapeRendering();
+     //   GameApp.drawTexture(textureName,x,y);
+
+    }
+}
+
