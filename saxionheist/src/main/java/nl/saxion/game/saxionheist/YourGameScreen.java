a4fcc902175ec92d9f9f59cd55572c8d622e3346
Bobbo <131258264+BobboBoi@users.noreply.github.com>
2025-11-27 13:27:02 +0100

    Add ScoreManager (#1)
    * Add ScoreManager
    
    * Add pausing and optimise score calculation

--- saxionheist/src/main/java/nl/saxion/game/saxionheist/YourGameScreen.java
+++ saxionheist/src/main/java/nl/saxion/game/saxionheist/YourGameScreen.java
@@ -1,13 +1,10 @@
-package nl.saxion.game.yourgamename;
+package nl.saxion.game.saxionheist;
 
-import com.badlogic.gdx.Input;
 import nl.saxion.gameapp.GameApp;
 import nl.saxion.gameapp.screens.ScalableGameScreen;
 
 public class YourGameScreen extends ScalableGameScreen {
-    String randomBoxColor = "violet-500";
-    String randomBackgroundColor = "black";
-    Box centeredBox = new Box();
+    ScoreManager scoreManager = null;
 
     public YourGameScreen() {
         super(1280, 720);
@@ -15,47 +12,21 @@ public class YourGameScreen extends ScalableGameScreen {
 
     @Override
     public void show() {
-        randomBoxColor = getRandomColor();
-        centeredBox.width = 400;
-        centeredBox.height = 400;
+        scoreManager = new ScoreManager();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
 
-        // Calculate where the box would be (we draw it in the center of the world)
-        float boxX = getWorldWidth() / 2 - centeredBox.width / 2;
-        float boxY = getWorldHeight() / 2 - centeredBox.height / 2;
+        // Clear
+        GameApp.clearScreen();
 
-        if (GameApp.isButtonJustPressed(Input.Buttons.LEFT)) {
-            float mouseX = getMouseX();
-            float mouseY = getMouseY();
-
-            if (mouseX > boxX && mouseX < boxX + centeredBox.width && mouseY > boxY && mouseY < boxY + centeredBox.height) {
-                // If we pressed the box, then change the color of the box
-                randomBoxColor = getRandomColor();
-            } else {
-                // Otherwise change the color of the background
-                randomBackgroundColor = getRandomColor();
-            }
-        }
-
-        // Draw elements
-        GameApp.clearScreen(randomBackgroundColor);
-        GameApp.startShapeRenderingFilled();
-        GameApp.drawRect(boxX, boxY, centeredBox.width, centeredBox.height, randomBoxColor);
-        GameApp.endShapeRendering();
-
-    }
-
-    private String getRandomColor() {
-        int randomIndex = (int)GameApp.random(0, GameApp.getAllColors().length-1);
-        return GameApp.getAllColors()[randomIndex];
+        scoreManager.render(delta);
     }
 
     @Override
     public void hide() {
-
+        scoreManager.dispose();
     }
 }
