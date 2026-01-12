ee457375b420dde937c4a8c5055d9025b31126e2
Bobbo <131258264+BobboBoi@users.noreply.github.com>
2026-01-12 21:02:51 +0100

    Add character selection scree (#24)

--- saxionheist/src/main/java/nl/saxion/game/Main.java
+++ saxionheist/src/main/java/nl/saxion/game/Main.java
@@ -1,5 +1,6 @@
 package nl.saxion.game;
 
+import nl.saxion.game.saxionheist.CharacterSelectScreen;
 import nl.saxion.game.saxionheist.YourGameScreen;
 import nl.saxion.game.saxionheist.MainMenuScreen;
 import nl.saxion.gameapp.GameApp;
@@ -8,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
         // Add screens
         GameApp.addScreen("MainMenuScreen", new MainMenuScreen());
+        GameApp.addScreen("CharacterSelectScreen", new CharacterSelectScreen());
         GameApp.addScreen("YourGameScreen", new YourGameScreen());
 
         // Start game loop and show main menu screen
-        GameApp.start("Your Game Name", 800, 450, 60, false, "MainMenuScreen");
+        GameApp.start("SaxionHeist", 800, 450, 60, false, "MainMenuScreen");
     }
 }
