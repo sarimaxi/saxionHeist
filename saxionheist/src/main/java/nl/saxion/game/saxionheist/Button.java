e04664ce9dca51c85fa0f6fe05f405d1a8745928
alexandratudor842 <alexandratudor842@gmail.com>
2026-01-16 00:42:21 +0100

    Changed the CharacterSelectScreen appearance, made the buttons more dynamic, and updated the game so that it starts with the CharacterSelectScreen instead of the initial one (#42)

--- saxionheist/src/main/java/nl/saxion/game/saxionheist/Button.java
+++ saxionheist/src/main/java/nl/saxion/game/saxionheist/Button.java
@@ -11,6 +11,11 @@ public class Button {
 
     private static final int mouseMargin = 1;
 
+    private boolean hovered = false;
+
+    private static final String COLOR_NORMAL = "saxionGreenBtn";
+    private static final String COLOR_HOVER = "saxionGreenBtnHover";
+
     public Button(float x, float y, float width, float height) {
         this.x = x;
         this.y = y;
@@ -28,46 +33,85 @@ public class Button {
 
     public static void init() {
         if (!GameApp.hasFont("ButtonFont"))
-            GameApp.addFont("ButtonFont", "fonts/basic.ttf", 40);
+            GameApp.addFont("ButtonFont", "fonts/basic.ttf", 48);
 
-        if (!GameApp.hasFont("ButtonSfx"))
+        if (!GameApp.hasFont("TitleFont"))
+            GameApp.addFont("TitleFont", "fonts/basic.ttf", 110);
+
+        if (!GameApp.hasFont("SubFont"))
+            GameApp.addFont("SubFont", "fonts/basic.ttf", 56);
+
+        if (!GameApp.hasFont("SmallFont"))
+            GameApp.addFont("SmallFont", "fonts/basic.ttf", 34);
+
+        if (!GameApp.hasSound("ButtonSfx"))
             GameApp.addSound("ButtonSfx", "sounds/button.wav");
+
+        if (!GameApp.hasColor(COLOR_NORMAL))
+            GameApp.addColor(COLOR_NORMAL, 0, 166, 81, 150);
+
+        if (!GameApp.hasColor(COLOR_HOVER))
+            GameApp.addColor(COLOR_HOVER, 0, 220, 110, 210);
     }
 
     public static void dispose() {
         if (GameApp.hasFont("ButtonFont"))
             GameApp.disposeFont("ButtonFont");
 
-        if (GameApp.hasFont("ButtonSfx"))
+        if (GameApp.hasFont("TitleFont"))
+            GameApp.disposeFont("TitleFont");
+
+        if (GameApp.hasFont("SubFont"))
+            GameApp.disposeFont("SubFont");
+
+        if (GameApp.hasFont("SmallFont"))
+            GameApp.disposeFont("SmallFont");
+
+        if (GameApp.hasSound("ButtonSfx"))
             GameApp.disposeSound("ButtonSfx");
     }
 
-    public void render(float delta) {
+    public boolean isHovered(float worldWidth, float worldHeight) {
+        Vector2 mousePos = GameApp.getMousePositionInWindow();
+        Vector2 mouseUV = new Vector2(mousePos.x / GameApp.getWindowWidth(), mousePos.y / GameApp.getWindowHeight());
+
+        float mx = mouseUV.x * worldWidth;
+        float my = mouseUV.y * worldHeight;
+
+        return GameApp.rectOverlap(
+                x - width / 2, y - height / 2, width, height,
+                mx - mouseMargin, my - mouseMargin, mouseMargin * 2, mouseMargin * 2
+        );
+    }
+
+    public void render(float delta, float worldWidth, float worldHeight) {
+        hovered = isHovered(worldWidth, worldHeight);
+
+        float s = hovered ? 1.06f : 1.0f;
+        float w = width * s;
+        float h = height * s;
+
+        GameApp.enableTransparency();
         GameApp.startShapeRenderingFilled();
-        GameApp.drawRect(x - width / 2, y - height / 2, width, height, "amber-200");
+        GameApp.drawRect(x - w / 2, y - h / 2, w, h, hovered ? COLOR_HOVER : COLOR_NORMAL);
         GameApp.endShapeRendering();
+        GameApp.disableTransparency();
 
         GameApp.startSpriteRendering();
-        GameApp.drawTextCentered(font, text, x, y, "black");
+        GameApp.drawTextCentered(font, text, x, y, "white");
         GameApp.endSpriteRendering();
     }
 
-    public boolean isPressed(Vector2 worldSize) {
-        return isPressed(worldSize.x, worldSize.y);
-    }
-
     public boolean isPressed(float worldWidth, float worldHeight) {
-        Vector2 mousePos = GameApp.getMousePositionInWindow();
-        Vector2 mouseUV =  new Vector2(mousePos.x / GameApp.getWindowWidth(),mousePos.y /  GameApp.getWindowHeight());
+        if (!hovered)
+            hovered = isHovered(worldWidth, worldHeight);
 
-        if (GameApp.rectOverlap(x - width / 2, y - height / 2, width, height,mouseUV.x * worldWidth - mouseMargin,mouseUV.y * worldHeight - mouseMargin,mouseMargin * 2,mouseMargin * 2) &&
-            GameApp.isButtonPressed(Input.Buttons.LEFT)) {
-
+        if (hovered && GameApp.isButtonPressed(Input.Buttons.LEFT)) {
             GameApp.playSound("ButtonSfx");
             return true;
         }
 
         return false;
-
     }
 }
+
